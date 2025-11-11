8f01ebca5797c60363ecff7269271e4488d54e31
viitorfernandess <vitorazevedofer007@gmail.com>
2025-11-11 16:07:39 -0300

    feat(auth): implementar rota de registro de usuário

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,9 +1,23 @@
-module.exports = {
-//POST /auth/register
-    register: (req, res) => {
+const users = require('../models/users')
 
+module.exports = {
+    //POST /auth/register
+    register: (req, res) => {
+        const { name, email, password } = req.body
+
+        if (typeof name !== 'string' || typeof email !== 'string' || typeof password !== 'string') {
+            return res.status(400).json({ message: 'Invalid fields.' })
+        }
+
+        const registeredUser = users.registerUser(name, email, password)
+        if(!registeredUser) {
+            return res.status(400).json({ message: 'Email already in use.' })
+        }
+
+        res.status(201).json(registeredUser)
     },
-//POST /auth/register
+    
+    //POST /auth/register
     login: (req, res) => {
 
     }
