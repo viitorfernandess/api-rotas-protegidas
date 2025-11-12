6158df19f66a00ba214de104df77f6453d53de1a
viitorfernandess <vitorazevedofer007@gmail.com>
2025-11-12 14:56:37 -0300

    feat: adiciona a rota POST /auth/login no authController

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -10,15 +10,27 @@ module.exports = {
         }
 
         const registeredUser = users.registerUser(name, email, password)
-        if(!registeredUser) {
+        if (!registeredUser) {
             return res.status(400).json({ message: 'Email already in use.' })
         }
 
         res.status(201).json(registeredUser)
     },
-    
-    //POST /auth/register
-    login: (req, res) => {
 
+    //POST /auth/login
+    login: (req, res) => {
+        const { email, password } = req.body
+
+        if (typeof email !== 'string' || typeof password !== "string") {
+            return res.status(400).json({ message: 'Invalid fields.' })
+        }
+
+        const user = users.findByEmail(email)
+
+        if (!user) return res.status(404).json({ message: 'User not found.' })
+
+        if (user.password !== password) {
+            return res.status(400).json({ message: 'Invalid credentials.' })
+        }
     }
 }
