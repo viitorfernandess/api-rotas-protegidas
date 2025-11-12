3fb3ef4068034924529e75aaa58bec95b4104dcf
viitorfernandess <vitorazevedofer007@gmail.com>
2025-11-12 15:17:43 -0300

    feat(routes): adiciona rota POST /auth/login no arquivo routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const authController =  require('./controllers/auth-controller')
 const router = express.Router()
 
 router.post( '/auth/register', authController.register)
+router.post('/auth/login', authController.login)
 
 module.exports = router
