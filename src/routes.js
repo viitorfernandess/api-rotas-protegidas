1cc58d59005c0f264d833efa2e18d98f302cd5f7
viitorfernandess <vitorazevedofer007@gmail.com>
2025-11-18 14:21:36 -0300

    feat: adiciona rota com autenticação opcional

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 const express =  require('express')
 const authController =  require('./controllers/auth-controller')
 const welcomeController = require('./controllers/welcome-controller')
+const { optionalAuth } = require('./middlewares/auth-middleware')
 
 const router = express.Router()
 
 router.post( '/auth/register', authController.register)
 router.post('/auth/login', authController.login)
 
-router.get('/welcome', welcomeController.welcome)
+router.get('/welcome', optionalAuth, welcomeController.welcome)
 
 module.exports = router
