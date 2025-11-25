cc82805a71d640b891eeba052b13b74381a75b99
viitorfernandess <vitorazevedofer007@gmail.com>
2025-11-25 14:54:42 -0300

    feat(rotas): adiciona middlewares ensureAuth e ensureAdmin na rota GET /users

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
-const express =  require('express')
-const authController =  require('./controllers/auth-controller')
+const express = require('express')
+const authController = require('./controllers/auth-controller')
 const welcomeController = require('./controllers/welcome-controller')
-const { optionalAuth } = require('./middlewares/auth-middleware')
+const { optionalAuth, ensureAuth, ensureAdmin } = require('./middlewares/auth-middleware')
 const usersController = require('./controllers/users-controller')
 
 const router = express.Router()
 
-router.post( '/auth/register', authController.register)
+router.post('/auth/register', authController.register)
 router.post('/auth/login', authController.login)
 
 router.get('/welcome', optionalAuth, welcomeController.welcome)
 
-router.get('/users', usersController.index)
+router.get('/users', ensureAuth, ensureAdmin, usersController.index)
 
 module.exports = router
